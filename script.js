c688feadabbe18d825a34dc2b1129942fdfbc877
Lucas Melo <luucasmell@gmail.com>
2019-07-23 17:49:15 -0300

    Adicionar exemplo de par ou impar

---
+++ script.js
@@ -0,0 +1,26 @@
+// let idade = 25;
+
+// idade = idade + 100;
+// idade += 100;
+
+// idade = idade - 2;
+// idade -= 2;
+
+// idade = idade / 5;
+// idade /= 5;
+
+// idade = idade * 18;
+// idade *= 18;
+
+// console.log(idade);
+
+let numero = Number(prompt("Digite um número"));
+
+if(numero % 2 === 0){
+    console.log(`O número ${numero} é par`);
+} 
+else{
+    console.log(`O número ${numero} é ímpar`);
+}
+
+
